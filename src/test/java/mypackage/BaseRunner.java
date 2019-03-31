30f8b6edea0a811059977ceea07ebaf908b27183
Gluk87 <diamond-city@yandex.ru>
2019-04-01 01:09:27 +0300

    Commit 2

--- src/test/java/mypackage/BaseRunner.java
+++ src/test/java/mypackage/BaseRunner.java
@@ -9,13 +9,14 @@ import java.util.concurrent.TimeUnit;
 public class BaseRunner {
     WebDriver driver;
     private String browserName;
+    String baseUrl = "https://www.tinkoff.ru/career/vacancies/";
+    String baseUrlTariff = "https://www.tinkoff.ru/mobile-operator/tariffs/";
 
     @Before
     public void setUp() {
-       // System.setProperty("browser","opera");
+        System.setProperty("browser","chrome");
         browserName = System.getProperty("browser");
         driver = getDriver();
-        String baseUrl = "https://www.tinkoff.ru/career/vacancies/";
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
